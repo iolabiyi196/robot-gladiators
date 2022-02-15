e6b418475a90ce5d50002ad63083a4e8b1bf4c13
ismahel olabiyi <ismail.olabiyi@gmail.com>
2022-02-15 15:15:42 -0500

    added more lines

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?")
+    // Note the lack of quotations marked around the playerName
+window.alert("Welcome to the game, " + playerName);
+
+//  this creates a function named "fighter"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
