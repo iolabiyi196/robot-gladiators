a9f3a7d152596a29a4c999af13d6e9c01b2db304
ismahel olabiyi <ismail.olabiyi@gmail.com>
2022-02-19 18:35:12 -0500

    fixed prompt bugs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,34 @@
+var fightOrSkip = function() {
+    // ask player if they'd like to fight or skip using fightOrSkip function
+    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+    // conditional recursive function call
+    if (promptFight === "" || promptFight === "null") {
+        window.alert("You need to provide a valid answer. please try again.")
+        return fightOrSkip();
+    }
+
+    // if player picks "skip" confirm and then stop the loop
+    if (promptFight === "skip" || promptFight === "SKIP") {
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        // if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+            // subtract money from playerMoney for skipping
+            playerInfo.money = playerInfo.money - 10;
+            shop();
+        }
+    }
+}
+
 var fight = function(enemy) {
     // repeat and execute as long as the enemy-robot is alive
     while (playerInfo.health > 0 && enemy.health > 0) {
         // fight function statements
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+        promptFight = promptFight.toLowerCase();
 
         // if player choses to fight, then fight
         if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -34,8 +60,9 @@ var fight = function(enemy) {
             } else {
                 window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
             }
-            // if player choses to skip
-        } else if (promptFight === "skip" || promptFight === "SKIP") {
+        }
+        // if player choses to skip
+        else if (promptFight === "skip" || promptFight === "SKIP") {
             // confirm player wants to skip
             var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
